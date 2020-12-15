5b4f82bab38850781a8595190ffe48827664c0f9
Kelly <renkelei.kelly@gmail.com>
2020-12-15 19:52:46 +0200

    adding the title for each corlor counter

--- src/components/ColorCounter.jsx
+++ src/components/ColorCounter.jsx
@@ -1,10 +1,11 @@
 import React from "react";
-import {  Button, View } from "react-native";
+import { Text, Button, View } from "react-native";
 
 const ColorCounter = ({ color, onIncrease, onDecrease }) => {
 
   return (
     <View>
+      <Text>Adjust the {color} color</Text>
       <Button title={`Increse ${color} color`} onPress={()=>{onIncrease()}} />
       <Button title={`Reduce ${color} color`} onPress={()=>{onDecrease()}}/>
     </View>
