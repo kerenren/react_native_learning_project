50726d067b418ba9747000fcac1be9d651e851c8
Kelly <renkelei.kelly@gmail.com>
2020-12-20 22:56:37 +0200

    adding box screen to home screen

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -33,6 +33,10 @@ const HomeScreen = ({ navigation }) => {
         onPress={() => navigation.navigate("Text")}
         title="Go to Text input demo"
       />
+      <Button
+        onPress={() => navigation.navigate("Box")}
+        title="Go to Box demo"
+      />
     </View>
   );
 };
